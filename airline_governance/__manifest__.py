a97480d163e2d85903475412981019bf1c081060
Ravi Bhingradiya <ravb@odoo.com>
2022-12-28 19:02:37 +0530

    Initial commit

---
+++ airline_governance/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Airlines',
+    'author': "Ravi Bhingradiya",
+    'data' : [
+            'security/ir.model.access.csv',
+            'views/airline_model_view.xml',
+            'views/airline_menus.xml',
+    ],
+
+    'demo' : [ 
+            'data/airline_demo_data.xml'
+    ],
+    'application' : True,
+    'installable': True,
+
+}
---
+++ airline_governance/models/airline_model.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+class airlineModel(models.Model):
+    _name = "airline.model"
+    _description = "Airline Model"
+
+    name = fields.Char('name', required = True)
+    description = fields.Text('Description')
+    type = fields.Selection(string="Type", selection = [('domestic','Domestic'), ('international','International')])
+    arrival_date_time = fields.Datetime('Arrival', required = True)
+    depart_date_time = fields.Datetime('Departure', required = True)
+    arrival_airport = fields.Char('Arrival Airport', required = True)
+    depart_airport = fields.Char('Departure Airport', required = True)
+    total_distance = fields.Integer('Total Distance (km)' , required = True)
+    gate_no = fields.Char('Gate No.', required = True)
+    price = fields.Integer("Total Price ", required = True)
+    active = fields.Boolean('Active', default = True)
