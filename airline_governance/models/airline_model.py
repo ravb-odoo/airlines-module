727e79d068eba8d98befbc8eb2f9c8892cb636ff
Ravi Bhingradiya <ravb@odoo.com>
2023-01-09 18:12:43 +0530

    form design and relation

--- airline_governance/models/airline_model.py
+++ airline_governance/models/airline_model.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 class airlineModel(models.Model):
     _name = "airline.model"
     _description = "Airline Model"
+    _rec_name = 'depart_airport_id'
 
     name = fields.Char('Name', required = True)
     description = fields.Text('Description')
@@ -11,8 +12,9 @@ class airlineModel(models.Model):
     arrival_date_time = fields.Datetime('Arrival', required = True)
     depart_date_time = fields.Datetime('Departure', required = True)
     arrival_airport = fields.Char('Arrival Airport', required = True)
-    depart_airport = fields.Char('Departure Airport', required = True)
+    depart_airport_id = fields.Many2one('airport.model', string= 'Departure Airport', required = True)
     total_distance = fields.Integer('Total Distance (km)' , required = True)
     gate_no = fields.Char('Gate No.', required = True)
     price = fields.Integer("Total Price ", required = True)
     active = fields.Boolean('Active', default = True)
+    passenger_ids = fields.One2many('passenger.model', 'flight_id')
